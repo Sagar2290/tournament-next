56ec49f4c4d9743e437b7b3816eab9891bdf7345
sagar <bhagwatsagar2290@gmail.com>
2025-03-20 00:43:09 +0530

    added login,logout,tables(User, Event, EventImage)

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,4 +1,16 @@
-export default function Page() {
+import { getServerSession } from "next-auth"
+import { redirect } from 'next/navigation'
+
+export default async function Page() {
+
+  const session = await getServerSession();
+
+  console.log(session)
+
+  if (!session?.user) {
+    redirect('/');
+  }
+
   return (
     <div>dashboard</div>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -13,13 +13,22 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { NavUser } from "@/components/nav-user"
 
 export default function Header() {
   return (
-    <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
+    <header className="flex justify-between sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
       <SidebarTrigger className="-ml-1" />
       <Separator orientation="vertical" className="mr-2 h-4" />
+      
+      <div className="max-w-40">
 
+      <NavUser user={{
+        name: "shadcn",
+        email: "m@example.com",
+        avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZQdHyyrnf31L6mDHY72cGoNXd_lzO8AHv1Asoj3Vtb2cBPcspyi_Fl3R1Ar1RjdtcRhk&usqp=CAU",
+      }} />
+      </div>
     </header>
   );
 }
